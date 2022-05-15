c62140a0e5ce1bf02d6a4d72936cdc0d4746e0cc
Phil Daum <philgdaum@gmail.com>
2022-05-15 13:59:02 -0700

    werkin on otha page with js

---
+++ js/nicksarray.js
@@ -0,0 +1,17 @@
+var items = {
+    coins: [
+        { name: "linkedin", "description": ["linkedin coin"] },
+        { name: "github", "description": ["github coin"] },
+        { name: "youtube", "description": ["youtube coin"] },
+    ],
+
+    keys: [
+        //standard keys
+        { name: "gold", "description": ["gold key"] },
+        { name: "silver", "description": ["silver key"] },
+        //secret keys
+        { name: "brown", "description": ["the weird"] },
+        { name: "red", "description": ["rickroll"] },
+        { name: "black", "description": ["the special key"] },
+    ]
+}
