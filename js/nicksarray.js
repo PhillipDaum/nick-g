e739cc2c481c5b35082e62c3ecadf1ac640eda32
Phil Daum <philgdaum@gmail.com>
2022-05-16 16:19:09 -0700

    did I not commit this

--- js/nicksarray.js
+++ js/nicksarray.js
@@ -1,17 +1,1 @@
-var items = {
-    coins: [
-        { name: "linkedin", "description": ["linkedin coin"] },
-        { name: "github", "description": ["github coin"] },
-        { name: "youtube", "description": ["youtube coin"] },
-    ],
-
-    keys: [
-        //standard keys
-        { name: "gold", "description": ["gold key"] },
-        { name: "silver", "description": ["silver key"] },
-        //secret keys
-        { name: "brown", "description": ["the weird"] },
-        { name: "red", "description": ["rickroll"] },
-        { name: "black", "description": ["the special key"] },
-    ]
-}
+// in development
